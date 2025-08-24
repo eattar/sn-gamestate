18af79170c8e14cc3a0b89c4ab29e3cae965f2e0
Ehsan Attar <riz69ric@rhrk.uni-kl.de>
2025-08-24 11:43:54 +0200

    Add UnifiedBackboneModule import to sn_gamestate.__init__ to ensure TrackLab can discover it

--- sn_gamestate/__init__.py
+++ sn_gamestate/__init__.py
@@ -3,3 +3,8 @@ try:
     __version__ = metadata.version('sn-gamestate')
 except metadata.PackageNotFoundError:
     __version__ = None
+
+# Import our custom modules so TrackLab can discover them
+from .calibration.unified_backbone import UnifiedBackboneModule
+
+__all__ = ['UnifiedBackboneModule']
